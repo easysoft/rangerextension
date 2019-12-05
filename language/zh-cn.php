4783263d2f52d585cfe576d65a6792f56dfaf8a5
chunsheng <chunsheng@cnezsoft.com>
2019-12-06 03:20:58 +0800

    Update lang files

--- language/zh-cn.php
+++ language/zh-cn.php
@@ -3432,7 +3432,9 @@ $lang->block->params->value = '参数值';
 $lang->block->createBlock        = '添加区块';
 $lang->block->editBlock          = '编辑区块';
 $lang->block->ordersSaved        = '排序已保存';
+$lang->block->reset              = '恢复默认';
 $lang->block->confirmRemoveBlock = '确定移除区块【{0}】吗？';
+$lang->block->confirmResetLayout = '确定恢复默认布局吗？';
 
 $lang->block->allEntries  = '所有应用';
 $lang->block->dynamic     = '最新动态';
@@ -3443,17 +3445,17 @@ $lang->block->gridOptions[4] = '右侧';
 
 $lang->block->default['oa']['1']['title'] = '日历';
 $lang->block->default['oa']['1']['block'] = 'attend';
-$lang->block->default['oa']['1']['grid']  = 6;
+$lang->block->default['oa']['1']['grid']  = 4;
 
 $lang->block->default['oa']['2']['title'] = '系统公告';
 $lang->block->default['oa']['2']['block'] = 'announce';
-$lang->block->default['oa']['2']['grid']  = 4;
+$lang->block->default['oa']['2']['grid']  = 8;
 
 $lang->block->default['oa']['2']['params']['num'] = 6;
 
 $lang->block->default['proj']['3']['title'] = '指派给我的任务';
 $lang->block->default['proj']['3']['block'] = 'task';
-$lang->block->default['proj']['3']['grid']  = 4;
+$lang->block->default['proj']['3']['grid']  = 8;
 
 $lang->block->default['proj']['3']['params']['num']     = 15;
 $lang->block->default['proj']['3']['params']['orderBy'] = 'id_desc';
@@ -3470,7 +3472,7 @@ $lang->block->default['proj']['4']['params']['status']  = 'doing';
 
 $lang->block->default['crm']['1']['title'] = '我的订单';
 $lang->block->default['crm']['1']['block'] = 'order';
-$lang->block->default['crm']['1']['grid']  = 4;
+$lang->block->default['crm']['1']['grid']  = 8;
 
 $lang->block->default['crm']['1']['params']['num']     = 15;
 $lang->block->default['crm']['1']['params']['orderBy'] = 'id_desc';
@@ -3479,7 +3481,7 @@ $lang->block->default['crm']['1']['params']['status']  = array();
 
 $lang->block->default['crm']['2']['title'] = '我的合同';
 $lang->block->default['crm']['2']['block'] = 'contract';
-$lang->block->default['crm']['2']['grid']  = 4;
+$lang->block->default['crm']['2']['grid']  = 8;
 
 $lang->block->default['crm']['2']['params']['num']     = 15;
 $lang->block->default['crm']['2']['params']['orderBy'] = 'id_desc';
@@ -3502,7 +3504,7 @@ $lang->block->default['cash']['1']['params'] = array();
 
 $lang->block->default['cash']['2']['title'] = '账目';
 $lang->block->default['cash']['2']['block'] = 'trade';
-$lang->block->default['cash']['2']['grid']  = 4;
+$lang->block->default['cash']['2']['grid']  = 8;
 
 $lang->block->default['cash']['2']['params']['num']     = 15;
 $lang->block->default['cash']['2']['params']['orderBy'] = 'id_desc';
@@ -3516,7 +3518,7 @@ $lang->block->default['cash']['3']['params']['orderBy'] = 'id_desc';
 
 $lang->block->default['team']['1']['title'] = '最新博客';
 $lang->block->default['team']['1']['block'] = 'blog';
-$lang->block->default['team']['1']['grid']  = 4;
+$lang->block->default['team']['1']['grid']  = 8;
 
 $lang->block->default['team']['1']['params']['num'] = 6;
 
@@ -3538,7 +3540,7 @@ $lang->block->default['sys']['1'] = $lang->block->default['oa']['1'];
 $lang->block->default['sys']['1']['source'] = 'oa';
 $lang->block->default['sys']['2']['title']  = '最新动态';
 $lang->block->default['sys']['2']['block']  = 'dynamic';
-$lang->block->default['sys']['2']['grid']   = 6;
+$lang->block->default['sys']['2']['grid']   = 4;
 $lang->block->default['sys']['2']['source'] = '';
 $lang->block->default['sys']['3'] = $lang->block->default['oa']['2'];
 $lang->block->default['sys']['3']['source'] = 'oa';
